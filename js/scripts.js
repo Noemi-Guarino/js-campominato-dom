e368b014ed1e4d0d48756f634aab12a8ae7f978b
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2023-12-14 18:57:20 +0100

    compleate

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@
 let containercell = document.querySelector(".containercell"); 
 
 let score = 0;
-
+let k = 0; // imposto una flag che mi da valore 0 (che è la condizione iniziale prima di iniziare a giocare)
 // agg la funzione al bottone
     let btnGenerate = document.getElementById("generate");
     btnGenerate.addEventListener ("click", 
@@ -38,21 +38,26 @@ let score = 0;
                     cell.classList.add('is-bomb'); //AGG UNA CLASSE PER RICHIAMARLO
                 }
 
-// creo un avvenimento cosi che cliccando sulla cella la colora di blu (aggiungendo la classe che andrò a stilizzare nel css nel caso in cui ci sia bisogno)
-
+// creo un avvenimento cosi che cliccando sulla cella la colora di blu (aggiungendo la classe che andrò a stilizzare nel css ) 
                 cell.addEventListener("click", function (){
-                    //CONDIZIONE IN CUI DICO SE CLICCO LA CELLA CHE HA QUESTA CLASSE MI DICE CHE HO PERSO                                     
-                    if(this.classList.contains('is-bomb')) { //CLASSE
-                        this.classList.add('red_bomb');
-                        alert("Hai perso!")
-                    } else {
-                        this.classList.add('winner');
-                        score++
-                        console.log("il tuo punteggio è" + score);
-                        if (score == 100 - 16){
-                            alert("Hai vinto!");
-                            alert("il tuo punteggio è" + score);
+                    // condizione in cui cambia la mia flag in base se perdo o vicno
+                    if (k == 0) {                                
+                        if(this.classList.contains('is-bomb')) { //richiamo la classe precedentemente data
+                            this.classList.add('red_bomb');//SE CLICCO LA CELLA CHE HA QUESTA CLASSE MI DICE CHE HO PERSO
+                            k = 1; //la flag da valore iniziale cambia perchè ("devo smettere di giocare dopo che ho perso")
+                            alert("Hai perso!");
+                        } else {
+                            this.classList.add('winner');
+                            score++
+                            console.log("il tuo punteggio è" + score);
+                            if (score == 100 - 16){
+                                alert("Hai vinto!");
+                                alert("il tuo punteggio è" + score);
+                            }
                         }
+
+                    }else{
+                        return;
                     }
                 });
             } 
